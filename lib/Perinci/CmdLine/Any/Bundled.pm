0744ce0864a4455c91990f971740824076d42525
perlancar (@pc-office) <perlancar@gmail.com>
2015-05-15 18:35:31 +0700

    Release v0.04

--- lib/Perinci/CmdLine/Any/Bundled.pm
+++ lib/Perinci/CmdLine/Any/Bundled.pm
@@ -1,6 +1,6 @@
 package Perinci::CmdLine::Any::Bundled;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 1;
 # ABSTRACT: Perinci::CmdLine::Any (bundled dist)
@@ -12,7 +12,7 @@ Perinci::CmdLine::Any::Bundled - Perinci::CmdLine::Any (bundled dist)
 
 =head1 VERSION
 
-0.03
+0.04
 
 
 =head1 DESCRIPTION
